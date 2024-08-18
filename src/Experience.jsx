f60d790fb7ca2ed8bc881ad6162f8c69de792723
Raffaele Spanò <nemoraffa@gmail.com>
2024-08-18 21:22:34 +0200

    feat: add more controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,14 +3,16 @@ import { useControls } from 'leva'
 
 export default function Experience()
 {
-    const { spherePosition } = useControls({
+    const { spherePosition, sphereColor, visible } = useControls({
         spherePosition: {
             value: { x: -2, y: 0 }, // enabling the z value results in losing the joystick control
             min: -4,
             max: 4,
             step: 0.01,
             joystick: 'invertY' // this fixes the inverted Y axis while using the joystick so for example when dragging the joystick up, the default behaviour makes the object goes down and vice versa, this fixes the issue
-        }
+        },
+        sphereColor: '#ff0000', // alpha in these colors won't work because of how Three.js handles the colors
+        visible: true
     });
 
     return <>
@@ -20,9 +22,9 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position={ [ spherePosition.x, spherePosition.y, 0 ] }>
+        <mesh visible={visible} position={ [ spherePosition.x, spherePosition.y, 0 ] }>
             <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color={sphereColor} />
         </mesh>
 
         <mesh position-x={ 2 } scale={ 1.5 }>
