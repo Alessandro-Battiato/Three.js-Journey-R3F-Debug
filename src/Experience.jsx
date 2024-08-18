4a6ab973761a96e27b85e11d6b99c8cd98ea884c
Raffaele Spanò <nemoraffa@gmail.com>
2024-08-18 20:59:52 +0200

    fix: fix joystick control issue

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,10 +5,11 @@ export default function Experience()
 {
     const { spherePosition } = useControls({
         spherePosition: {
-            value: { x: -2, y: 0, z: 0 }, // enabling the z value results in losing the joystick control
+            value: { x: -2, y: 0 }, // enabling the z value results in losing the joystick control
             min: -4,
             max: 4,
-            step: 0.01
+            step: 0.01,
+            joystick: 'invertY' // this fixes the inverted Y axis while using the joystick so for example when dragging the joystick up, the default behaviour makes the object goes down and vice versa, this fixes the issue
         }
     });
 
@@ -19,7 +20,7 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position={ [ spherePosition.x, spherePosition.y, spherePosition.z ] }>
+        <mesh position={ [ spherePosition.x, spherePosition.y, 0 ] }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
